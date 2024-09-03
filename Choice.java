aea65c042becb345f130c68130ac6aa977092596
yutokikuchi <yuuto.kikuchi.sys23@morijyobi.ac.jp>
2024-09-03 22:34:08 +0900

    クラスとコンストラクタの作成 close #6 close #7

---
+++ Choice.java
@@ -0,0 +1,11 @@
+package quiz;
+
+public class Choice {
+    private int index;
+    private String text;
+
+    public Choice(int index, String text) {
+        this.index = index;
+        this.text = text;
+    }
+}
---
+++ Quiz.java
@@ -0,0 +1,17 @@
+package quiz;
+
+import java.util.List;
+
+public class Quiz {
+    private int quizNumber;
+    private String mainText;
+    private List<Choice> choices;
+    private int answer;
+
+    public Quiz(int quizNumber, String mainText, List<Choice> choices, int answer) {
+        this.quizNumber = quizNumber;
+        this.mainText = mainText;
+        this.choices = choices;
+        this.answer = answer;
+    }
+}
